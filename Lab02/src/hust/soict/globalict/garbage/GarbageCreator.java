c46a622fa88ba9f4038ac9d6ef30b4f95ace177a
ThisIsMakago <phamhuuhieu28@gmail.com>
2026-04-16 22:53:31 +0700

    Add garbage test

---
+++ Lab02/src/hust/soict/globalict/garbage/GarbageCreator.java
@@ -0,0 +1,28 @@
+package hust.soict.globalict.garbage;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) {
+        String filename = "test.exe"; 
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        try 
+        {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        } 
+        catch (Exception e) {
+            System.out.println("File not found.");
+            return;
+        }
+        
+        startTime = System.currentTimeMillis();
+        String outputString = ""; 
+        for (byte b : inputBytes) {
+            outputString += (char) b; 
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
