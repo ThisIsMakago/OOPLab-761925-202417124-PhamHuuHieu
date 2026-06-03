3f3288f8c5b44f50c9789cb598c9bdcdd1bc84c8
ThisIsMakago <phamhuuhieu28@gmail.com>
2026-06-03 22:13:59 +0700

    Complete view store screen

---
+++ Lab02/src/hust/soict/globalict/aims/screen/customer/controller/ItemController.java
@@ -0,0 +1,51 @@
+package hust.soict.globalict.aims.screen.customer.controller;
+
+import hust.soict.globalict.aims.media.Media;
+import hust.soict.globalict.aims.media.Playable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class ItemController {
+
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Label lblCost;
+
+    @FXML
+    private Label lblTitle;
+
+    private Media media;
+
+    @FXML
+    void btnAddToCartClicked(ActionEvent event) {
+        System.out.println("Add to cart clicked!");
+    }
+
+    @FXML
+    void btnPlayClicked(ActionEvent event) {
+        System.out.println("Play clicked!");
+    }
+
+    public void setData(Media media) {
+        this.media = media;
+        lblTitle.setText(media.getTitle());
+        lblCost.setText(media.getCost() + " $");
+        
+        if (media instanceof Playable) {
+            btnPlay.setVisible(true);
+        } 
+        else {
+            btnPlay.setVisible(false);
+            HBox.setMargin(btnAddToCart, new Insets(0, 0, 0, 60));
+        }
+    }
+}
---
+++ Lab02/src/hust/soict/globalict/test/screen/customer/store/TestViewStoreScreen.java
@@ -0,0 +1,38 @@
+package hust.soict.globalict.test.screen.customer.store;
+
+import hust.soict.globalict.aims.media.DigitalVideoDisc;
+import hust.soict.globalict.aims.store.Store;
+import hust.soict.globalict.aims.screen.customer.controller.ViewStoreController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestViewStoreScreen extends Application {
+    private static Store store;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final String STORE_FXML_FILE_PATH = "/hust/soict/globalict/aims/screen/customer/view/Store.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(STORE_FXML_FILE_PATH));
+        
+        ViewStoreController viewStoreController = new ViewStoreController(store);
+        fxmlLoader.setController(viewStoreController);
+        Parent root = fxmlLoader.load();
+        
+        primaryStage.setTitle("Store");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        store = new Store();
+        
+        store.addMedia(new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f));
+        store.addMedia(new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f));
+        store.addMedia(new DigitalVideoDisc("Aladin", "Animation", 18.99f));
+        
+        launch(args);
+    }
+}
